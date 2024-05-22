f9d7bcac262502cdea03d5745b5a95aabd827051
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-22 22:35:54 +0900

    can share folder too

--- DriverUtils.js
+++ DriverUtils.js
@@ -1,20 +1,43 @@
-function shareFiles(driveFileId, email) {
-  try {
-    let file = DriveApp.getFileById(driveFileId)
-    file.addViewer(email)
-    let fileUrl = file.getUrl()
-    return fileUrl
-  } catch (e){
-    console.error('공유 오류 발생:', e.toString())
+function shareFiles(driveId, email) {
+  let driveObject = getDriveObject(driveId);
+  if (!driveObject) {
+    console.error("유효하지 않은 파일 또는 폴더 ID입니다.");
+    return "";
   }
-  return ""
+
+  try {
+    driveObject.addViewer(email);
+    return driveObject.getUrl();
+  } catch (e) {
+    console.error("공유 오류 발생:", e.toString());
+    return "";
+  }
 }
 
-function unshareExpiredFiles(driveFileId, email) {
-  try {
-    let file = DriveApp.getFileById(driveFileId);
-    file.removeViewer(email);
-  } catch (e) {
-    console.error('공유 해제 오류 발생:', e.toString());
+function unshareExpiredFiles(driveId, email) {
+  let driveObject = getDriveObject(driveId);
+  if (!driveObject) {
+    console.error("유효하지 않은 파일 또는 폴더 ID입니다.");
+    return;
   }
+
+  try {
+    driveObject.removeViewer(email);
+  } catch (e) {
+    console.error("공유 해제 오류 발생:", e.toString());
+  }
+}
+
+function getDriveObject(driveId) {
+  let result;
+  try {
+    return (result = DriveApp.getFileById(driveId));
+  } catch (e) {
+    try {
+      result = DriveApp.getFolderById(driveId);
+    } catch (e) {
+      console.error("공유 오류 발생:", e.toString());
+    }
+  }
+  return result;
 }
